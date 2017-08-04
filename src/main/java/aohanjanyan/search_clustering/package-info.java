883de13bbe7e54dfc9feb3743925a8c054b369d9
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2017-08-04 16:25:49 +0300

    Add more documentation

--- src/main/java/aohanjanyan/search_clustering/package-info.java
+++ src/main/java/aohanjanyan/search_clustering/package-info.java
@@ -1,4 +1,9 @@
 /**
- * TODO
+ * Package with clustering algorithm described in
+ * "Clustering Search Engine Query Log Containing Noisy Clickthroughs"
+ * by Wing Shun Chan, Wai Ting Leung and Dik Lun Lee.
+ *
+ * Also provides simple CLI application to run it on
+ * "500k User Session Collection".
  */
 package aohanjanyan.search_clustering;
